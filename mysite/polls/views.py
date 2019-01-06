ec90917ae83286737ea5f85e5f59e8c5b5214764
Pablo Muguerza <pmuguer@gmail.com>
2019-01-06 16:52:42 -0300

    Tutorial de Django, part 3b

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,19 +1,21 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
 
 from polls.models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template("polls/index.html")
     context = {
         'latest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    q = Question.objects.get(id=question_id)
-    return HttpResponse("Question %s = %s" % (question_id, q))
+    try:
+        q = Question.objects.get(id=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {'question': q})
 
 def results(request, question_id):
     return HttpResponse("You are looking at the results of question %s"
