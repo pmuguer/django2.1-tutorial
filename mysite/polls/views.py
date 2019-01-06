7934cc164361db6362ecf59fcf40eecaeafeb9b2
Pablo Muguerza <pmuguer@gmail.com>
2019-01-06 17:10:09 -0300

    Tutorial de Django, part 3c

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-from polls.models import Question, Choice
+from polls.models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -11,10 +11,7 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    try:
-        q = Question.objects.get(id=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    q = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {'question': q})
 
 def results(request, question_id):
