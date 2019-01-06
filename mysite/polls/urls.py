95e2ada995ae03585eb772661ad695a919faa2f0
Pablo Muguerza <pmuguer@gmail.com>
2019-01-06 16:37:49 -0300

    Tutorial de Django, part 3a

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from polls import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index),
+    path('<int:question_id>/', views.detail),
+    path('<int:question_id>/results/', views.results),
+    path('<int:question_id>/vote/', views.vote),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,23 @@
 from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+from polls.models import Question, Choice
+
 def index(request):
-    return HttpResponse("Hello World of Django! You are on the polls index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template("polls/index.html")
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    q = Question.objects.get(id=question_id)
+    return HttpResponse("Question %s = %s" % (question_id, q))
+
+def results(request, question_id):
+    return HttpResponse("You are looking at the results of question %s"
+        % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s" % question_id)
